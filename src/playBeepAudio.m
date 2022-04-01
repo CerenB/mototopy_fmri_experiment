3f703ad209694101b03098b676e5f47a68d97a1e
Ceren <battal.ceren@gmail.com>
2022-04-01 20:16:14 +0200

    make two channel - for future possibility

--- src/playBeepAudio.m
+++ src/playBeepAudio.m
@@ -4,11 +4,11 @@ function onset  = playBeepAudio(cfg)
     soundData = cfg.soundData;
     fieldName = 'eventNoTarget';
     
-    sound = soundData.(fieldName);
-
+    soundCh1 = soundData.(fieldName);
+    soundCh2 = soundCh1;
     
     % Start the sound presentation
-    PsychPortAudio('FillBuffer', cfg.audio.pahandle, sound);
+    PsychPortAudio('FillBuffer', cfg.audio.pahandle, [soundCh1; soundCh2]);
     PsychPortAudio('Start', cfg.audio.pahandle);
     onset = GetSecs;
 
